5dcb79a5047fa0a4721b924a1eb49bcd933d3f45
mikatekomata <100471670+mikatekomata@users.noreply.github.com>
2023-10-13 14:13:08 +0200

    Updated server

--- server.java
+++ server.java
@@ -1,7 +1,22 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
+
+import static src.GenerateCertificate.encodeCertificate;
 
 public class server {
+    private static List<PrintWriter> clientWriters = new ArrayList<>();
+
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(12345);
@@ -16,40 +31,140 @@ public class server {
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
                 Thread thread = new Thread(clientHandler);
                 thread.start();
+
+                // Create a PrintWriter for this client and add it to the list
+                PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                clientWriters.add(clientWriter);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // Helper method to broadcast a message to all connected clients
+    public static void broadcastMessage(String message) {
+        for (PrintWriter writer : clientWriters) {
+            writer.println(message);
+        }
+    }
 }
 
 class ClientHandler implements Runnable {
     private Socket clientSocket;
+    private PrintWriter out;
+
+    public static Map<String, X509Certificate> clientCertificates = new HashMap<>();
+
 
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
+        try {
+            this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            closeEverything(this.clientSocket, out);
+        }
     }
 
     public void run() {
         try {
-            // Create input and output streams for communication
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            // Read and print messages from the client
             String message;
-            while ((message = in.readLine()) != null) {
-                System.out.println("Received from client: " + message);
 
-                // Send a response back to the client
-                out.println("Server received your message: " + message);
+            while ((message = in.readLine()) != null) {
+                // Check for "@key" prefix to store the key
+                // generate the certificate here
+                if (message.startsWith("@key:")) {
+                    // Split the message to get the client name and encoded public key
+                    String[] parts = message.split(":", 3);
+
+                    if (parts.length == 3) {
+                        String clientName = parts[1];
+                        String encodedPublicKey = parts[2];
+                        // generate the certificate
+                        PublicKey publicKey = decodePublicKey(encodedPublicKey);
+                        X509Certificate certificate = GenerateCertificate.issueCertificate(publicKey);
+
+                        // Directly store the base64 encoded public key in the map
+                        clientCertificates.put(clientName, certificate);
+//                        clientPublicKeys.put(clientName, encodedPublicKey);
+                        System.out.println("Stored certificate for client: " + clientName + ":" + clientCertificates.get(clientName));
+                    } else {
+                        System.out.println("Invalid key message format");
+                    }
+                }
+                // Check for "@getKey" request
+                // get certificate, verify it and then send it to the client
+                else if (message.startsWith("@getKey:")) {
+                    String[] parts = message.split(":", 2);
+                    if (parts.length == 2) {
+                        String requestedClientName = parts[1];
+                        X509Certificate cert = clientCertificates.get(requestedClientName);
+
+                        if (cert != null) {
+                            if (GenerateCertificate.verifyCertificate(cert)) {
+                                // Send the already base64-encoded public key to the requesting client
+                                // get the certificate,
+                                // get the public key from it
+                                // and encode key
+                                out.println("@keyResponse:" + requestedClientName + ":" + encodeCertificate(cert));
+
+                                System.out.println("@keyResponse:" + requestedClientName + ":" + encodeCertificate(cert));
+                            }
+                            else {
+                                out.println("Certificate not valid");
+                            }
+                        } else {
+                            // Optionally handle the case where the requested public key doesn't exist
+                            out.println("Err: Certificate for " + requestedClientName + " not found.");
+                        }
+                    }
+                } else {
+                    // Handle as a regular message
+                    server.broadcastMessage(message);
+                }
             }
 
-            // Close the connections
+            
             in.close();
-            out.close();
             clientSocket.close();
+        } catch (IOException e) {
+            closeEverything(clientSocket, out);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String encodedPublicKey(PublicKey publicKey) throws Exception {
+
+        byte[] publicKeyBytes = publicKey.getEncoded();
+        String EncodedPublicKey = Base64.getEncoder().encodeToString(publicKeyBytes);
+        return EncodedPublicKey;
+    }
+
+    public PublicKey decodePublicKey(String encodedPublicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // Decode the Base64-encoded public key into a byte array
+        byte[] publicKeyBytes = Base64.getDecoder().decode(encodedPublicKey);
+
+        // Create an X509EncodedKeySpec to represent the public key
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+
+        // Get a KeyFactory for the desired algorithm (e.g., RSA or EC)
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+        return publicKey;
+
+    }
+
+    public void closeEverything(Socket socket, PrintWriter writer) {
+        try {
+            if (writer != null) {
+                writer.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
